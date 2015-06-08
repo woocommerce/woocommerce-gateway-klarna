7df2210e3a2d51bf0ea9ba30aef5dda4ca673bd4
Slobodan Manic <slobodan.manic@gmail.com>
2015-06-08 10:03:17 +0200

    Updates update_klarna_order_edit_item so it only works in AJAX calls

--- classes/class-klarna-checkout.php
+++ classes/class-klarna-checkout.php
@@ -1743,27 +1743,30 @@ class WC_Gateway_Klarna_Checkout extends WC_Gateway_Klarna {
 	function update_klarna_order_edit_item( $orderid, $items ) {
 		$order = wc_get_order( $orderid );
 
-		// Check if auto cancellation is enabled and order is on hold so it can be edited
-		if ( 'yes' == $this->push_update && 'on-hold' == $order->get_status() ) {
-			// Check if order was created using this method
-			if ( $this->id == get_post_meta( $orderid, '_payment_method', true ) ) {
-				// Check if this order hasn't been cancelled or activated
-				if ( ! get_post_meta( $orderid, '_klarna_order_cancelled', true ) && ! get_post_meta( $orderid, '_klarna_order_activated', true ) ) {
-					$rno = get_post_meta( $orderid, '_klarna_order_reservation', true );
-					$country = get_post_meta( $orderid, '_billing_country', true );
+		// Only do this if in an AJAX call (not when saving the entire order)
+		if ( defined( 'DOING_AJAX' ) && DOING_AJAX ) {
+			// Check if auto cancellation is enabled and order is on hold so it can be edited
+			if ( 'yes' == $this->push_update && 'on-hold' == $order->get_status() ) {
+				// Check if order was created using this method
+				if ( $this->id == get_post_meta( $orderid, '_payment_method', true ) ) {
+					// Check if this order hasn't been cancelled or activated
+					if ( ! get_post_meta( $orderid, '_klarna_order_cancelled', true ) && ! get_post_meta( $orderid, '_klarna_order_activated', true ) ) {
+						$rno = get_post_meta( $orderid, '_klarna_order_reservation', true );
+						$country = get_post_meta( $orderid, '_billing_country', true );
 
-					$klarna = new Klarna();
-					$this->configure_klarna( $klarna, $country );
+						$klarna = new Klarna();
+						$this->configure_klarna( $klarna, $country );
 
-					$klarna_order = new WC_Gateway_Klarna_Order( $order, $klarna );
-					$klarna_order->add_addresses( $this->klarna_secret, $this->klarna_server );
-					$klarna_order->process_cart_contents();
-					$klarna_order->process_shipping();
-					// $klarna_order->set_estore_info();
-					$klarna_order->update_order( $rno );
-				}		
-			}	
-		}	
+						$klarna_order = new WC_Gateway_Klarna_Order( $order, $klarna );
+						$klarna_order->add_addresses( $this->klarna_secret, $this->klarna_server );
+						$klarna_order->process_cart_contents();
+						$klarna_order->process_shipping();
+						// $klarna_order->set_estore_info();
+						$klarna_order->update_order( $rno );
+					}		
+				}	
+			}
+		}
 	}
 
 
