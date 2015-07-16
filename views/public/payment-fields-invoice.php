2bab745ff09df7cb0f1c6480f39c5aea7a50a5fc
Slobodan Manic <slobodan.manic@gmail.com>
2015-07-16 10:21:41 +0200

    Fixes #106

--- views/public/payment-fields-invoice.php
+++ views/public/payment-fields-invoice.php
@@ -9,11 +9,6 @@
  */
 
 
-// Show klarna_warning_banner if NL
-if ( $this->klarna_helper->get_klarna_country() == 'NL' ) {
-	echo '<p><img src="' . $this->klarna_wb_img_checkout . '" class="klarna-wb" style="max-width:100%;float:none;max-height:none"/></p>';	
-}
-
 // Mobile or desktop browser
 if ( wp_is_mobile() ) {
 	$klarna_layout = 'mobile';
