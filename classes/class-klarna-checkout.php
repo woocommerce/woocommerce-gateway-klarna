aac819477d33eb6957dadc35b5c3439fa8251520
Slobodan Manic <slobodan.manic@gmail.com>
2015-12-10 09:49:12 +0100

    Fixes #188

--- classes/class-klarna-checkout.php
+++ classes/class-klarna-checkout.php
@@ -294,7 +294,7 @@ class WC_Gateway_Klarna_Checkout extends WC_Gateway_Klarna {
 			'posts_per_page' => -1,
 			'date_query'     => array(
 				array(
-					'before' => '2 days ago'
+					'before' => '2 weeks ago'
 				)
 			)
 
