7490aee6b05568b42deae0c102e9603e90d6de9d
Slobodan Manic <slobodan.manic@gmail.com>
2016-04-12 09:49:53 +0200

    Fixes AffiliateWP referrals information

--- classes/class-klarna-to-wc.php
+++ classes/class-klarna-to-wc.php
@@ -206,9 +206,6 @@ class WC_Gateway_Klarna_K2WC {
 			$order = $this->create_order();
 			update_post_meta( $order->id, '_kco_incomplete_customer_email', $customer_email, true );
 			$woocommerce->session->set( 'ongoing_klarna_order', $order->id );
-
-			// Other plugins need this hook
-			do_action( 'woocommerce_checkout_order_processed', $order->id, false );
 		}
 
 		// If there's an order at this point, proceed
@@ -253,6 +250,9 @@ class WC_Gateway_Klarna_K2WC {
 			} else {
 				update_post_meta( $order->id, '_klarna_api', 'v2' );
 			}
+
+			// Other plugins need this hook
+			do_action( 'woocommerce_checkout_order_processed', $order->id, false );
 
 			return $order->id;
 		}
