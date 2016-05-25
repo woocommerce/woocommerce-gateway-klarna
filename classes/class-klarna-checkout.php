5a9c42439dbc3ceebfafcc0e7ff0506b1938c3b6
Slobodan Manic <slobodan.manic@gmail.com>
2016-05-25 09:08:37 +0200

    Fixes label for shipping in KCO cart widget

--- classes/class-klarna-checkout.php
+++ classes/class-klarna-checkout.php
@@ -629,6 +629,7 @@ class WC_Gateway_Klarna_Checkout extends WC_Gateway_Klarna {
 
 		try {
 			$klarna_order->create( $create );
+			
 			if ( isset( $klarna_order['invoice'] ) ) {
 				add_post_meta( $order->id, '_klarna_order_invoice_recurring', $klarna_order['invoice'], true );
 				$order->add_order_note( __( 'Klarna subscription payment invoice number: ', 'woocommerce-gateway-klarna' ) . $klarna_order['invoice'] );
@@ -641,7 +642,6 @@ class WC_Gateway_Klarna_Checkout extends WC_Gateway_Klarna {
 		} catch ( Klarna_Checkout_ApiErrorException $e ) {
 			$order->add_order_note( sprintf( __( 'Klarna subscription payment failed. Error code %s. Error message %s', 'woocommerce-gateway-klarna' ), $e->getCode(), utf8_encode( $e->getMessage() ) ) );
 
-			// error_log( var_export( $e, true ) );
 			return false;
 		}
 	}
@@ -1444,7 +1444,7 @@ class WC_Gateway_Klarna_Checkout extends WC_Gateway_Klarna {
 						<?php if ( ! empty( $available_methods ) ) { ?>
 							<?php if ( 1 === count( $available_methods ) ) {
 								$method = current( $available_methods );
-								echo wp_kses_post( wc_cart_totals_shipping_method_label( $method ) ); ?>
+								echo wp_kses_post( $method->get_label() ); ?>
 								<input type="hidden" name="shipping_method[<?php echo esc_attr( $index ); ?>]"
 								       data-index="<?php echo esc_attr( $index ); ?>"
 								       id="shipping_method_<?php echo esc_attr( $index ); ?>"
@@ -2260,6 +2260,7 @@ class WC_Gateway_Klarna_Checkout extends WC_Gateway_Klarna {
 			WC()->session->set( 'klarna_show_kco', false );
 			return false;
 		}
+
 
 		// If no Klarna country is set - return.
 		if ( empty( $this->klarna_country ) || empty( $this->klarna_eid ) || empty( $this->klarna_secret ) ) {
