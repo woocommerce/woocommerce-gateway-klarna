78e75a099dee447e51b877f31fcfb5a285a23567
Slobodan Manic <slobodan.manic@gmail.com>
2016-06-21 11:41:16 +0200

    Adds SKU check in Klarna Checkout validate

--- classes/class-klarna-validate.php
+++ classes/class-klarna-validate.php
@@ -40,9 +40,15 @@ class WC_Gateway_Klarna_Order_Validate {
 		} elseif ( is_array( $data['cart']['items'] ) ) {
 			$cart_items = $data['cart']['items']; // V2
 		}
+
 		foreach ( $cart_items as $cart_item ) {
 			if ( 'physical' == $cart_item['type'] ) {
-				$cart_item_product = $this->get_product_by_sku( $cart_item['reference'] );
+				// Get product by SKU or ID
+				if ( wc_get_product_id_by_sku( $cart_item['reference'] ) ) {
+					$cart_item_product = wc_get_product( wc_get_product_id_by_sku( $cart_item['reference'] ) );
+				} else {
+					$cart_item_product = wc_get_product( $cart_item['reference'] );
+				}
 
 				if ( ! $cart_item_product->has_enough_stock( $cart_item['quantity'] ) ) {
 					$all_in_stock = false;
@@ -65,62 +71,6 @@ class WC_Gateway_Klarna_Order_Validate {
 			}
 		}
 	} // End function validate_checkout_listener
-	
-	
-	/**
-	 * Return product object from SKU
-	 * Checks in regular products first. Then in product variations.
-	 *
-	 * @since 2.1.8
-	 */
-	public function get_product_by_sku( $sku ) {
-		
-		$product_id = '';
-		
-		// Search in regular products
-		$args = array(
-			'post_type'		=>	'product',
-			'meta_key' => '_sku',
-			'meta_value' => $sku,
-			'meta_compare' => '=',
-	
-		);
-		
-		$my_query = new WP_Query( $args );
-		if( $my_query->have_posts() ) {
-			while( $my_query->have_posts() ) {
-				$my_query->the_post();
-				$product_id = get_the_id();
-			} // end while
-		} // end if
-		wp_reset_postdata();
-		
-		// If $product_id is empty - check in product variations
-		if( empty($product_id) ) {
-			$args = array(
-			'post_type'		=>	'product_variation',
-			'meta_key' => '_sku',
-			'meta_value' => $sku,
-			'meta_compare' => '=',
-	
-		);
-		
-		$my_query = new WP_Query( $args );
-		if( $my_query->have_posts() ) {
-			while( $my_query->have_posts() ) {
-				$my_query->the_post();
-				$product_id = get_the_id();
-			} // end while
-		} // end if
-		wp_reset_postdata();
-		}
-		
-		if ( $product_id ) {
-			return wc_get_product( $product_id );
-		} else {
-			return null;
-		}
-	} // End function get_product_by_sku
 
 }
 $wc_gateway_klarna_order_validate = new WC_Gateway_Klarna_Order_Validate();
