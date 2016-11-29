b099e7b3d4ea021908c60a246ca82c012948057b
Slobodan Manic <slobodan.manic@gmail.com>
2016-11-29 12:06:24 +0100

    Fixes character encoding issue on order update (#17)

--- classes/class-klarna-order.php
+++ classes/class-klarna-order.php
@@ -65,29 +65,35 @@ class WC_Gateway_Klarna_Order {
 	function add_addresses() {
 		$order  = $this->order;
 		$klarna = $this->klarna;
+		if ( get_post_meta( $order->id, '_billing_address_2', true ) ) {
+			$billing_address = get_post_meta( $order->id, '_billing_address_1', true ) . ' ' . get_post_meta( $order->id, '_billing_address_2', true );
+		}
+		if ( get_post_meta( $order->id, '_billing_address_2', true ) ) {
+			$shipping_address = get_post_meta( $order->id, '_shipping_address_1', true ) . ' ' . get_post_meta( $order->id, '_shipping_address_2', true );
+		}
 		$billing_addr = new KlarnaAddr( get_post_meta( $order->id, '_billing_email', true ), // Email address
 			'', // Telephone number, only one phone number is needed
-			get_post_meta( $order->id, '_billing_phone', true ), // Cell phone number
-			get_post_meta( $order->id, '_billing_first_name', true ), // First name (given name)
-			get_post_meta( $order->id, '_billing_last_name', true ), // Last name (family name)
+			utf8_decode( get_post_meta( $order->id, '_billing_phone', true ) ), // Cell phone number
+			utf8_decode( get_post_meta( $order->id, '_billing_first_name', true ) ), // First name (given name)
+			utf8_decode( get_post_meta( $order->id, '_billing_last_name', true ) ), // Last name (family name)
 			'', // No care of, C/O
-			get_post_meta( $order->id, '_billing_address_1', true ), // Street address
-			get_post_meta( $order->id, '_billing_postcode', true ), // Zip code
-			get_post_meta( $order->id, '_billing_city', true ), // City
-			get_post_meta( $order->id, '_billing_country', true ), // Country
+			utf8_decode( $billing_address ), // Street address
+			utf8_decode( get_post_meta( $order->id, '_billing_postcode', true ) ), // Zip code
+			utf8_decode( get_post_meta( $order->id, '_billing_city', true ) ), // City
+			utf8_decode( get_post_meta( $order->id, '_billing_country', true ) ), // Country
 			null, // House number (AT/DE/NL only)
 			null // House extension (NL only)
 		);
 		$shipping_addr = new KlarnaAddr( get_post_meta( $order->id, '_shipping_email', true ), // Email address
 			'', // Telephone number, only one phone number is needed
-			get_post_meta( $order->id, '_shipping_phone', true ), // Cell phone number
-			get_post_meta( $order->id, '_shipping_first_name', true ), // First name (given name)
-			get_post_meta( $order->id, '_shipping_last_name', true ), // Last name (family name)
+			utf8_decode( get_post_meta( $order->id, '_shipping_phone', true ) ), // Cell phone number
+			utf8_decode( get_post_meta( $order->id, '_shipping_first_name', true ) ), // First name (given name)
+			utf8_decode( get_post_meta( $order->id, '_shipping_last_name', true ) ), // Last name (family name)
 			'', // No care of, C/O
-			get_post_meta( $order->id, '_shipping_address_1', true ), // Street address
-			get_post_meta( $order->id, '_shipping_postcode', true ), // Zip code
-			get_post_meta( $order->id, '_shipping_city', true ), // City
-			get_post_meta( $order->id, '_shipping_country', true ), // Country
+			utf8_decode( $shipping_address ), // Street address
+			utf8_decode( get_post_meta( $order->id, '_shipping_postcode', true ) ), // Zip code
+			utf8_decode( get_post_meta( $order->id, '_shipping_city', true ) ), // City
+			utf8_decode( get_post_meta( $order->id, '_shipping_country', true ) ), // Country
 			null, // House number (AT/DE/NL only)
 			null // House extension (NL only)
 		);
@@ -829,7 +835,6 @@ class WC_Gateway_Klarna_Order {
 	 *
 	 * @param $orderid
 	 * @param bool $itemid
-	 * @param bool $skip_address
 	 */
 	function update_order( $orderid, $itemid = false ) {
 		$order       = wc_get_order( $orderid );
