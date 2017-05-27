54589ab1c8fbc7cfc850b5919332c625445cca3a
Niklas Högefjord <niklas@krokedil.se>
2017-05-27 21:27:38 +0200

    Add feature to be able to hide coupon form via shortcode attribute

--- classes/class-klarna-shortcodes.php
+++ classes/class-klarna-shortcodes.php
@@ -232,6 +232,7 @@ class WC_Gateway_Klarna_Shortcodes {
 		$atts = shortcode_atts( array(
 			'col'           => '',
 			'order_note'    => '',
+			'coupon_form'   => '',
 			'hide_columns'  => '',
 			'only_shipping' => ''
 		), $atts );
@@ -288,7 +289,7 @@ class WC_Gateway_Klarna_Shortcodes {
 			$atts = WC()->session->get( 'kco_widget_atts' );
 		} else {
 			// Set empty defaults
-			$atts = array( 'order_note' => '', 'hide_columns' => '', 'only_shipping' => '' );
+			$atts = array( 'order_note' => '', 'coupon_form' => '', 'hide_columns' => '', 'only_shipping' => '' );
 		}
 
 		do_action( 'kco_widget_before_calculation', $atts );
@@ -309,7 +310,9 @@ class WC_Gateway_Klarna_Shortcodes {
 			<?php do_action( 'kco_widget_before_coupon', $atts ); ?>
 
 			<!-- Coupons -->
-			<?php woocommerce_checkout_coupon_form(); ?>
+			<?php if ( 'hide' !== $atts['coupon_form'] ) { ?>
+				<?php woocommerce_checkout_coupon_form(); ?>
+			<?php }; ?>
 
 			<?php do_action( 'kco_widget_before_cart_items', $atts ); ?>
 
