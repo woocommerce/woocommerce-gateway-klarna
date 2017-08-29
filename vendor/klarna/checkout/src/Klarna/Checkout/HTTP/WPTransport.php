964b912c1fb3849e06f11857dc1b9be5ca9dc472
Slobodan Manic <slobodan.manic@gmail.com>
2017-08-29 13:01:26 +0200

    Adds better error logging in WPTransport.php

--- vendor/klarna/checkout/src/Klarna/Checkout/HTTP/WPTransport.php
+++ vendor/klarna/checkout/src/Klarna/Checkout/HTTP/WPTransport.php
@@ -105,6 +105,7 @@ class Klarna_Checkout_HTTP_WPTransport implements Klarna_Checkout_HTTP_Transport
 		$my_response = wp_remote_request( $req_url, $args );
 
 		if ( is_wp_error( $my_response ) ) {
+			error_log( 'wp_remote_request error in WPTransport.php' );
 			error_log( var_export( $my_response, true ) );
 
 			$error_code    = $my_response->get_error_code();
